0390256b3aaa56a441d622ae3a7be2f1fc61dfe5
bdell <brandon.demello@gmail.com>
2016-07-25 19:30:38 -0400

    removes babel generated files

--- spec/keyboard-logger_spec.js
+++ spec/keyboard-logger_spec.js
@@ -1,14 +1,28 @@
 'use strict';
 
 import KeyboardLogger from '../keyboard-logger.js';
+import path from 'path';
 
 let globals = {};
 
 describe('KeyboardLogger', () => {
     beforeEach(() => {
+        globals.defaultOpts = {
+            kbdPath: '/dev/input/by-path/platform-i8042-serio-0-event-kbd',
+            keyboardOutputDir: path.resolve(__dirname, 'keyboard')
+        };
         globals.keyboardLogger = new KeyboardLogger();
     });
     it('should be defined', () => {
         expect(globals.keyboardLogger).toBeDefined();
+    });
+    it('should initialize the opts values with defaults if not present', () => {});
+    it('should create the keyboard log output dir on write if not present', () => {});
+    it('should create the read stream', () => {});
+    it('should create the write stream', () => {});
+    describe('handleKeyboardEvent(buffer)', () => {
+        it('should activate the logger when l is pressed', () => {});
+        it('should kill the logger when k is pressed', () => {});
+        it('should log keyboard events when active', () => {});
     });
 });
--- src/keyboard-logger.js
+++ src/keyboard-logger.js
@@ -1,8 +1,41 @@
 'use strict';
 
+import _  from 'lodash';
+import path from 'path';
+import { fs as fse } from 'fs-extra';
+import fs from 'fs';
+import winston from 'winston';
+
 class KeyboardLogger {
     constructor(opts) {
-        this.opts = opts;
+        const defaultOpts = {
+            kbdPath: '/dev/input/by-path/platform-i8042-serio-0-event-kbd',
+            keyboardOutputDir: path.resolve(__dirname, 'keyboard')
+        };
+        this.active = false;
+        this.opts = _.assign(defaultOpts, opts);
+        this.readStream = fs.createReadStream(this.opts.kbdPath)
+                            .on('data', buffer => {
+                                this.handleKeyboardEvent(buffer);
+                            });
+        this.writeStream = new (winston.Logger)({
+            transports: [
+                new (winston.transports.Console)(),
+                new (winston.transports.File)({ filename: path.resolve(this.opts.keyboardOutputDir, `${ new Date(Date.now()).toISOString() }.input.log`) })
+            ]
+        });
+    }
+    handleKeyboardEvent(buffer) {
+        let eventData = { timeStamp: Date.now(), code: buffer.readUInt16LE(20), value: buffer.readInt32LE(44) };
+        if (eventData.code === 38 && eventData.value === 1 && !this.active) {
+            this.active = true;
+        }
+        if (eventData.code === 37 && eventData.value === 1 && this.active) {
+            this.active = false;
+        }
+        if (this.active) {
+            this.writeStream.log('info', eventData);
+        }
     }
 }
 
